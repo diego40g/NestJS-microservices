65a30d49d572bf11e19fcc99b6fca502a7d28b58
diego40g <diegopaznaula@hotmail.com>
2024-07-28 14:53:44 -0500

    chore: Update TasksController to handle task-related endpoints and add filtering functionality

--- task-management-application/src/tasks/tasks.service.ts
+++ task-management-application/src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
+import { Task, TaskStatus } from './models/task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/request/create-task.dto';
+import { GetTasksFilterDto } from './dto/request/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -8,6 +10,27 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+    }
+    return tasks;
   }
 
   createTask(title: string, description: string): Task {
@@ -19,5 +42,26 @@ export class TasksService {
     };
     this.tasks.push(task);
     return task;
+  }
+  createTaskDto(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
