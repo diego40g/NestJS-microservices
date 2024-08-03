7334a89927586ceea5b557aac7dc0071ed31dca6
Diego Ignacio Paz Naula <diegopaznaula@hotmail.com>
2024-08-02 20:28:54 -0500

    S1/task manger/dpaz/f001 (#2)
    * chore: remove unused code files
    
    * chore: Update npm dependencies for task-management-application
    
    * feat: Add TasksService to manage tasks
    
    * chore: Add TasksController to handle task-related endpoints
    
    * chore: Add TasksModule to handle task-related functionality
    
    * chore: Update app.module.ts and main.ts for configuration management
    
    * chore: Add Dockerfile and docker-compose.yaml for containerization
    
    * feat: Remove Task model and related code
    
    * feat: Add CreateTaskDto and GetTasksFilterDto for task management
    
    * chore: Update TasksController to handle task-related endpoints and add filtering functionality

---
+++ task-management-application/src/tasks/tasks.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './models/task.model';
+import { CreateTaskDto } from './dto/request/create-task.dto';
+import { GetTasksFilterDto } from './dto/request/get-tasks-filter.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/id')
+  getTaskById(@Body('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Get('/:id')
+  getTaskParamById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Get('/filter')
+  getTasksWithFilter(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilter(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
+
+  @Post('/dto')
+  createTaskDto(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTaskDto(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+}
