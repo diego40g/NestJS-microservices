df43917097a0d1c5e81ccc45ea6e9ff42735f73e
diego40g <diegopaznaula@hotmail.com>
2024-08-04 15:12:02 -0500

    feat: Add error handling for task not found in getTaskById method

--- task-management-application/src/tasks/tasks.service.ts
+++ task-management-application/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './models/task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/request/create-task.dto';
@@ -13,7 +13,16 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    // try to get task
+    const found = this.tasks.find((task) => task.id === id);
+
+    // if task not found, throw an error (404 not found)
+    if (!found) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    // otherwise, if task found, return it (200 ok)
+    return found;
   }
 
   getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
