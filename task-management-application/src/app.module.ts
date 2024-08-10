838cb3ef9337226ffe24e4af2387502ed0fb83bf
diego40g <diegopaznaula@hotmail.com>
2024-08-09 22:54:35 -0500

    feat: Add TaskEntityModule to app.module.ts for task entity management

--- task-management-application/src/app.module.ts
+++ task-management-application/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskEntityModule } from './task-entity/task-entity.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    TaskEntityModule,
   ],
 })
 export class AppModule {}
