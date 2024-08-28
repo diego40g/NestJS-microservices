9e06c1801f83c5f5edcaabbd1c7d7887418d13b2
diego40g <diegopaznaula@hotmail.com>
2024-08-27 22:33:54 -0500

    feat: Hash user password using bcrypt in createUser method

--- task-management-application/src/auth/users.repository.ts
+++ task-management-application/src/auth/users.repository.ts
@@ -6,6 +6,7 @@ import {
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
@@ -15,7 +16,13 @@ export class UsersRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    // Create salt of password
+    const salt = await bcrypt.genSalt();
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
