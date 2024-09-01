fe041e18e8ee3f3c7ffddcd6b0d9e6a3fc2d04da
diego40g <diegopaznaula@hotmail.com>
2024-09-01 13:08:38 -0500

    feat: Update sign-in endpoint to return access token in AuthController

--- task-management-application/src/auth/auth.module.ts
+++ task-management-application/src/auth/auth.module.ts
@@ -4,9 +4,31 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+    }),
+    /*JwtModule.register({
+      secret: 'secretDpKey',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),*/
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UsersRepository],
   exports: [UsersRepository],
